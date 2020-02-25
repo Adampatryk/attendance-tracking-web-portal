b4da539fd345e017d9880f41be29a97bb300edb6
Adam <psyak12@nottingham.ac.uk>
2020-02-25 15:47:48 +0000

    Switched to hosted DB and login working

--- login/views.py
+++ login/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            username = request.user.username
+            return HttpResponse("<h1>Hello " + username + "</h1>")
+        return render(request, 'login/login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login/login.html', {'form': form})
